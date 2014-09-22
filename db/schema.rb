d2b7cf9b00400fe2c8eeb97a6760bc9df9db0ba1
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-09-22 16:56:54 -0400

    Log in with username instead of email

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140922204049) do
+ActiveRecord::Schema.define(version: 20140922204555) do
 
   create_table "users", force: true do |t|
     t.string   "email",           null: false
     t.string   "password_digest", null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
