0cd15aab0ebd0324538dda592aed6fdc7eaf1391
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-09-23 15:33:47 -0400

    Refactor User model using concern

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,29 +1,9 @@
 class User < ActiveRecord::Base
+  include Following
+
   has_many :shouts
-
-  has_many :followed_user_relationships,
-    foreign_key: :follower_id,
-    class_name: "FollowingRelationship"
-  has_many :followed_users, through: :followed_user_relationships
-
-  has_many :follower_relationships,
-    foreign_key: :followed_user_id,
-    class_name: "FollowingRelationship"
-  has_many :followers, through: :follower_relationships
 
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
   validates :username, presence: true, uniqueness: true
-
-  def following?(user)
-    followed_user_ids.include?(user.id)
-  end
-
-  def follow(user)
-    followed_users << user
-  end
-
-  def unfollow(user)
-    followed_users.delete(user)
-  end
 end
